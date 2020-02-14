6e81badac19ebae85dbb2a03cb60c4573d33e542
Timur Koshel <timurkoshel@gmail.com>
2020-02-14 21:33:07 +0200

    Union-Find: quick find

---
+++ Algorithms/Algorithms/Structure/UnionFind/QuickFind.cs
@@ -0,0 +1,36 @@
+namespace Algorithms.Structure.UnionFind
+{
+    public class QuickFind
+    {
+        private int[] _ids;
+
+        public QuickFind(int n)
+        {
+            _ids = new int[n];
+
+            for (var i = 0; i < n; i++)
+            {
+                _ids[i] = i;
+            }
+        }
+
+        public bool Connected(int p, int q)
+        {
+            return _ids[p] == _ids[q];
+        }
+
+        public void Union(int p, int q)
+        {
+            var pId = _ids[p];
+            var qId = _ids[q];
+
+            for (var i = 0; i < _ids.Length; i++)
+            {
+                if (_ids[i] == pId)
+                {
+                    _ids[i] = qId;
+                }
+            }
+        }
+    }
+}
