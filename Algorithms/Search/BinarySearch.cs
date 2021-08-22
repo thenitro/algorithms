6b915f5c5d1a0075ee12d170c6aa8fa77b72f118
Tymur Koshel <timurkoshel@gmail.com>
2021-08-22 16:28:46 +0300

    Binary search overflow fix

--- Algorithms/Search/BinarySearch.cs
+++ Algorithms/Search/BinarySearch.cs
@@ -8,23 +8,24 @@ namespace Algorithms.Search
         public static int Search(int[] array, int item, int left = -1, int right = -2)
         {
             var l = left == -1 ? 0 : left;
-            var r = right == -1 ? array.Length : right;
+            var r = right == -2 ? array.Length : right;
             
-            while (l < r)
+            while (l <= r)
             {
-                var middle = (l + r) / 2;
+                var middle = l + (r - l) / 2;
                 
                 if (item > array[middle])
                 {
                     l = middle + 1;
-                    continue;
-                } else if (item < array[middle])
+                } 
+                else if (item < array[middle])
                 {
-                    r = middle;
-                    continue;
+                    r = middle - 1;
                 }
-                
-                return middle;
+                else
+                {
+                    return middle;
+                }
             }
             
             return -1;
